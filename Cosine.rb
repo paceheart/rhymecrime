1bfe7e186385eb8cda2a9ebcbaadaf22a7cad366
Pace Heart <38589027+paceheart@users.noreply.github.com>
2025-02-28 13:23:22 -0800

    minor optimization

--- Cosine.rb
+++ Cosine.rb
@@ -9,8 +9,8 @@ class Cosine
     return nil unless @vecB.is_a? Array
     return nil if @vecA.size != @vecB.size
     dot_product = 0
-    @vecA.zip(@vecB).each do |v1i, v2i|
-      dot_product += v1i * v2i
+    for i in 0..@vecA.length-1 do
+      dot_product += @vecA[i] * @vecB[i]
     end
     a = @vecA.map { |n| n ** 2 }.reduce(:+)
     b = @vecB.map { |n| n ** 2 }.reduce(:+)
