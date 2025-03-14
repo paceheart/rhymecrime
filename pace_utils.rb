31ae8b67548fdcd5c051c25209b248ad0de4f134
Pace Heart <38589027+paceheart@users.noreply.github.com>
2025-03-14 11:08:28 -0700

    lots of useful helpers/extensions

---
+++ pace_utils.rb
@@ -0,0 +1,143 @@
+#!/usr/bin/env ruby
+# useful Ruby utilities
+
+$debug_mode = false
+
+def debug(string)
+  if($debug_mode)
+    puts string
+  end
+end
+
+def boolean?(object)
+  object == !!object
+end
+
+class Array
+  # does ARRAY contain any duplicate items?
+  def duplicates?
+    return self.length != self.uniq.length
+  end
+
+  # How many non-nil values does ARRAY contain?
+  def non_nil_count
+    count = 0
+    self.each { |val| count += val ? 1 : 0 }
+    return count
+  end
+
+  # The sum of all numeric values in ARRAY
+  # To be precise, the sum of all values in ARRAY that respond to +
+  def sum_numeric
+    result = 0
+    for value in self
+      if value.respond_to?('+')
+        result += value
+      end
+    end
+    return result
+  end
+
+  # Assuming both ARRAY and OTHER are sorted using the same sort, return their intersection (also sorted)
+  def intersection_assuming_sorted(other)
+    result = []
+    i = 0
+    j = 0
+    until i >= self.length or j >= other.length do
+      case self[i] <=> other[j]
+      when -1
+        i += 1
+      when 1
+        j += 1
+      else
+        result.append(self[i])
+        i += 1
+        j += 1
+      end
+    end
+    return result
+  end
+end
+
+class IntegerArray < Array
+  def [](index)
+    super(index).to_i
+  end
+end
+
+class ArrayOfIntegerArrays < Array
+  def [](index)
+    result = super(index)
+    if result.nil?
+      result = IntegerArray.new
+      self[index] = result
+    end
+    return result
+  end
+end
+
+class File
+  def writeln(str)
+    self.write("#{str}\n")
+  end
+end
+
+class Hash
+  # appends VALUE to self[key]
+  # Assumes this hash is a hash of arrays (or something that supports append).
+  def push(key, value)
+    self[key] = self[key].append(value)
+  end
+  
+  # Increments each key in the hash by its corresponding value in other_hash
+  # assumes both this hash and other_hash have numeric values
+  def hash_increment_all(other_hash)
+    for key, num in other_hash
+      unless num.nil? # robustify against nils
+        self[key] += num
+      end
+    end
+  end
+
+  # Create a new Hash whose default for each key is a new empty array
+  def self.new_hash_of_arrays
+    Hash.new { |h, k| h[k] = [] }
+  end
+
+  # Create a new Hash whose default for each key is a new empty IntegerArray
+  def self.new_hash_of_integer_arrays
+    Hash.new { |h, k| h[k] = IntegerArray.new }
+  end
+
+  # Create a new Hash whose default for each key is a new empty Hash
+  def self.new_hash_of_hashes
+    Hash.new { |h, k| h[k] = Hash.new }
+  end
+
+  # Destructively sort all HASH's values
+  def sort_values
+    for key, val in self
+      val = val.sort!
+      self[key] = val
+    end
+  end
+end
+
+class String
+  def ensure_trailing(final_char)
+    self[-1] == final_char ? self : self + final_char
+  end
+
+  def ensure_trailing_newline
+    self.ensure_trailing("\n")
+  end
+  
+  def ensure_trailing_slash
+    self.ensure_trailing('/')
+  end
+
+  # How many alphabetic characters are in this string?
+  def count_alpha
+    self.count("a-zA-Z")
+  end
+end
