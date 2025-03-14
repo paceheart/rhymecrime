71c7287c43639ca9dc43146ebea3cf4693b18a67
Pace Heart <38589027+paceheart@users.noreply.github.com>
2025-03-14 11:10:18 -0700

    helper script for debugging why two words are related (or not)

---
+++ debug.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require_relative 'crime'
+
+word1 = ARGV[0]
+word2 = ARGV[1]
+raise "Must specify two words to debug" unless word1 && word2
+puts "Debugging '#{word1}' and '#{word2}'"
+
+wet = IndexedWetCorpus.new
+$debug_mode = true
+wet.print_cooccurrence(word1, word2)
+puts wet.cooccurrence_documents(word1, word2)
