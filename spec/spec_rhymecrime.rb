a04816bf04495ceb3bac23255a226df4b9b3b4ad
Pace Heart <pace.heart@later.com>
2026-04-03 10:39:15 -0700

    run just the rarity tests for now

--- spec/spec_rhymecrime.rb
+++ spec/spec_rhymecrime.rb
@@ -5,8 +5,8 @@ TEST_FOR_SURPRISING_SUCCESSES = true
 
 NOT_WORKING = false; #don't edit this one
 
-#require_relative 'spec_rarity'
+require_relative 'spec_rarity'
 #require_relative 'spec_rhyme'
-require_relative 'spec_related'
+#require_relative 'spec_related'
 #require_relative 'spec_similar_rhymes'
 #require 'stackprof'
